87cc7e80fdaca9327143c6c05d091758f9b2833d
Francisco Mestizo <A01731549@itesm.mx>
2022-10-25 12:11:50 -0500

    Bootstrap and initial component

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-button>Hola amigo</b-button>
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col>1 of 3</b-col>
+                <b-col>2 of 3</b-col>
+                <b-col>3 of 3</b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
