36c7110b1d8200fb518cb4f92776326bc6f963d7
fazfaz7 <adrian.faz123@gmail.com>
2022-10-26 12:17:49 -0500

    Nuevo Post

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
 </template>
 
 <script>
-import Login from './components/Login.vue'
-import Header from './components/Header.vue'
-import Registro from './components/Registro.vue'
 
 
 export default {
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Home from "./components/Home.vue"
 import Login from "./components/Login.vue"
 import Registro from "./components/Registro.vue"
+import NuevoPost from "./components/NuevoPost.vue"
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -31,6 +32,10 @@ const router = new VueRouter({
     {
       path: '/registro',
       component: Registro
+    },
+    {
+      path: '/crearpost',
+      component: NuevoPost
     }
   ],
 
