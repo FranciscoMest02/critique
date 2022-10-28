75ff66c717f3938e3f056125e08acd483475a3f5
Francisco Mestizo <A01731549@itesm.mx>
2022-10-28 12:47:16 -0500

    config

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Critique/" : "/",
+};
