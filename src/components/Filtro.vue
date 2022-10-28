c0a0a59802ce0273dbec13a4d7a7da17e4f52fb8
Francisco Mestizo <A01731549@itesm.mx>
2022-10-27 21:53:55 -0500

    Cambios

--- src/components/Filtro.vue
+++ src/components/Filtro.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Header />
+        <NavBar />
         <b-container fluid>
             <b-row>
                 <b-col cols="3">
@@ -30,12 +30,12 @@ import axios from "axios"
 
 import Post from './Post.vue'
 import Sidebar from './Sidebar.vue'
-import Header from './Header.vue'
+import NavBar from './NavBar.vue'
 
 export default {
     name: 'Home',
     components: {
-        Post, Sidebar, Header
+        Post, Sidebar, NavBar
     },
     data: () => ({
         posts: null
